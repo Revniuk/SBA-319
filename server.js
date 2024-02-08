79944bdbaabef401c7054aee440acfea43e66341
Svitlana Revniuk <69059791+Revniuk@users.noreply.github.com>
2024-02-07 22:56:11 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const artworkRoutes = require('./routes/artworkRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb://localhost:27017/art_for_impact';
+
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // Start the server once MongoDB connection is established
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/artworks', artworkRoutes);
+app.use('/api/users', userRoutes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+});
